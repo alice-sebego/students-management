d7f9394a5ad87d2f079a60b1d80ca5b57c86157e
Alice Sebego <alice.etvotrecom@gmail.com>
2022-10-19 14:55:36 +0200

    feat: create footer of pages

---
+++ partial/footer.php
@@ -0,0 +1,16 @@
+<footer>
+    <div class="sub-footer">
+        <img class="logo" src="https://cdn.svgporn.com/logos/nightwatch.svg" alt="logo nightwatch for example">
+        <p class="copyright">2022 | Cook Study</p>
+    </div>
+    <div class="sub-footer title-footer">
+        <h3>Plateforme de gestion des étudiants</h3>
+    </div>
+    <div class="sub-footer">
+        <ul class="legacies">
+            <li class="legacy">Contact</li>
+            <li class="legacy">Mentions légales</li>
+            <li class="legacy">RGPD</li>
+        </ul>
+    </div>
+</footer>
--- partial/header.php
+++ partial/header.php
@@ -1,5 +1,5 @@
 <header>
-    <img src="https://cdn.svgporn.com/logos/apptentive.svg" alt="logo apptentive">
+    <img src="https://cdn.svgporn.com/logos/nightwatch.svg" alt="logo nightwatch for example">
     <nav>
         <ul>
             <li class="nav connection"><a href="login.php">Connexion</a></li>
