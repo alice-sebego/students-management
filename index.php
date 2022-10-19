c41a68f837e8115cb11c8212999bb3114f703492
Alice Sebego <alice.etvotrecom@gmail.com>
2022-10-19 14:24:46 +0200

    feat: create header of pages

--- index.php
+++ index.php
@@ -4,9 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/style.css">
     <title>Gestion des étudiants</title>
 </head>
 <body>
-    
+    <?php include_once "partial/header.php" ?>
+    <main class="main-content">
+        <h1>Bienvenue sur la plateforme 
+            <span class="sub-title">Gestion des Etudiants</span>
+        </h1>
+    </main>
+    <?php include_once "partial/footer.php" ?>
 </body>
 </html>
---
+++ partial/header.php
@@ -0,0 +1,9 @@
+<header>
+    <img src="https://cdn.svgporn.com/logos/apptentive.svg" alt="logo apptentive">
+    <nav>
+        <ul>
+            <li class="nav connection"><a href="login.php">Connexion</a></li>
+            <li class="nav logout"><a href="logout.php">Déconnexion</a></li>
+        </ul>
+    </nav>
+</header>
