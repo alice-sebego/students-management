dc9c801f2f0039f9867454d9bdea4bd853863b02
Alice Sebego <alice.etvotrecom@gmail.com>
2022-10-20 17:50:27 +0200

    feat: create a page for update a student's informations

---
+++ inc/get-student.php
@@ -0,0 +1,23 @@
+<?php
+if(isset($_GET['id'])) {
+
+    $idUser = $_GET['id'];
+    $is_valid_id = false;
+
+    $reqGet = "SELECT * FROM students WHERE id=$idUser";
+    $queryGet = $mysqli->query($reqGet);
+    $nbStudent = $queryGet->num_rows;
+    if($nbStudent > 0){
+        $is_valid_id = true;
+        while($row = $queryGet->fetch_array()){
+            $firstname = $row['firstname'];
+            $lastname = $row['lastname'];
+            $email = $row['email'];
+            $telephone = $row['telephone'];
+            $address = $row['address'];
+        }
+    } else {
+        $feedback = "Pas de compte associé à l'utilisateur n° $idUser";
+    }
+}
+?>
--- update.php
+++ update.php
@@ -1,12 +1,46 @@
 <?php 
 session_start();
 $title = "Modifier";
+include_once "inc/db.php";
+include_once "inc/get-student.php";
 ?>
 <!DOCTYPE html>
 <html lang="fr">
 <?php include_once "partial/head.php" ?>
 <body>
     <?php include_once "partial/header.php" ?>
+    <main class="main-content">
+        <h1>Modifier les informations d'un étudiant</h1>
+        <div class="container">
+            <?php if($is_valid_id): ?>
+            <form class="form-update" action="treatment-update.php" method="post">
+                <fieldset>
+                    <label for="firstname">Prénom  </label>
+                    <input type="text" name="firstname" id="firstname" value="<?php echo $firstname; ?>">
+                </fieldset>
+                <fieldset>
+                    <label for="lastname">Nom  </label>
+                    <input type="text" name="lastname" id="lastname" value="<?php echo $lastname; ?>">
+                </fieldset>
+                <fieldset>
+                    <label for="email">Email  </label>
+                    <input type="email" name="email" id="email" value="<?php echo $email; ?>">
+                </fieldset>
+                <fieldset>
+                    <label for="telephone">Téléphone  </label>
+                    <input type="tel" name="telephone" id="telephone" value="<?php echo $telephone; ?>">
+                </fieldset>
+                <fieldset>
+                    <label for="address">Adresse  </label>
+                    <input type="text" name="address" id="address" value="<?php echo $address; ?>">
+                </fieldset>
+                <input type="submit" name="submit" value="modifier">
+            </form>
+            <?php else: ?>
+                <p><?php echo $feedback; ?></p>
+            <?php endif; ?>    
+        </div>
+    </main>
     <?php include_once "partial/footer.php" ?>
 </body>
 </html>
